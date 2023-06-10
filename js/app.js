f17ba091b1eda4c33660e7dd0444bec5281af0fa
francojnieva <franco97nieva@hotmail.com>
2023-06-10 19:30:10 -0300

    :hammer: add drop event to the puzzle pieces

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 const partImg = document.querySelectorAll(".part-img")
-const sectionPuzzle = document.querySelectorAll(".section-puzzle")
+const puzzleDivs = document.querySelectorAll(".section-puzzle div")
 
 partImg.forEach(part => {
     part.addEventListener("dragstart", (e) => {
         part.classList.add("dragging")
-        e.dataTransfer.setData("class", part.id)
+        e.dataTransfer.setData("id", part.id)
     })
 
     part.addEventListener("dragend", () => {
         part.classList.remove("dragging")
     })
+})
+
+puzzleDivs.forEach(div => {
+  div.addEventListener("dragover", (e) => {
+    e.preventDefault()
+  })
+
+  div.addEventListener("drop", (e) => {
+    const draggedPartId = e.dataTransfer.getData("id")
+    // console.log(draggedPartId)
+    const draggedPart = document.getElementById(draggedPartId)
+    draggedPart.style.width = "80px"
+    div.appendChild(draggedPart)
+  })
 })
